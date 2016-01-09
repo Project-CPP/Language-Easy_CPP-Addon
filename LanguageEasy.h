efd32e8085d14f9dcb45303885e96a591660bb4b
JacobMuncy <jacobmuncy0@gmail.com>
2016-01-09 18:06:13 -0500

    Initialize Language Easy Plugin for CPP.

---
+++ LanguageEasy.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+
+#define MainDoor int main()
+#define Say cout <<
+#define SayEnd ;
+#define SuccessReturn return 0;
+#define Failreturn return 1;
+#define Getinput cin.get();
+#define PauseWindow cin.get();
+#define Integer int
+#define IntegerEnd ;
+#define Word string
+#define WordEnd ;
---
+++ YourProject.cpp
@@ -0,0 +1,15 @@
+#include "LanguageEasy.h"
+
+
+
+MainDoor
+{
+	Word Hey = "Hello" WordEnd
+	Integer Hello = 200 IntegerEnd
+	Say "Hey" SayEnd
+	Say "Hello" SayEnd
+	Say 2 + 2 SayEnd
+	PauseWindow
+	SuccessReturn
+	Say "Hello" SayEnd
+}
