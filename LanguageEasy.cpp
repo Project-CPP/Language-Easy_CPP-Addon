aafc74732949e2822f97de5a946d12983cecf713
JacobMuncy <jacobmuncy0@gmail.com>
2016-01-12 00:21:00 -0500

    Add Windows Battery Class. (OOP)
    Windows battery class simplifies the Windows API used to get the battery
    information. Using this you can get the battery percent, battery flag,
    and is the pc plugged in. There are methods for providing many data
    types, so in that case, people who use this wouldn't have to use
    stringstream or other ways of conversion from one data type to another.

--- LanguageEasy.cpp
+++ LanguageEasy.cpp
@@ -21,3 +21,103 @@ void WindowMessage::OutputWindow()
 	sprintf_s(MessageNameBuffer, MessageName.c_str());
 	MessageBoxA(NULL, MessageBuffer, MessageNameBuffer, MB_OK);
 }
+
+
+
+
+// WindowsBattery
+
+WindowsBattery::WindowsBattery(bool IsBatteryPresent)
+{
+
+}
+double WindowsBattery::Output_Battery_Flag_As_Double()
+{
+
+	double BatteryFlag = false;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		BatteryFlag = static_cast<double>(spsPwr.BatteryFlag);
+	}
+	return BatteryFlag;
+}
+int WindowsBattery::Output_Battery_Flag_As_Integer()
+{
+
+	int BatteryFlag = false;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		BatteryFlag = static_cast<int>(spsPwr.BatteryFlag);
+	}
+	return BatteryFlag;
+}
+bool WindowsBattery::Output_Battery_Flag_As_Boolean()
+{
+
+	bool BatteryFlag = false;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		BatteryFlag = static_cast<bool>(spsPwr.BatteryFlag);
+	}
+	return BatteryFlag;
+}
+string WindowsBattery::Output_Battery_Percentage_As_String()
+{
+	string BatteryPercent = "";
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		double Battery = static_cast<double>(spsPwr.BatteryLifePercent);
+		stringstream BatteryString;
+		BatteryString << static_cast<double>(spsPwr.BatteryLifePercent);
+		BatteryPercent = BatteryString.str();
+	}
+	return BatteryPercent;
+}
+double WindowsBattery::Output_Battery_Percentage_As_Double()
+{
+	double BatteryPercent = 0;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		BatteryPercent = static_cast<double>(spsPwr.BatteryLifePercent);
+	}
+	return BatteryPercent;
+}
+int WindowsBattery::Output_Battery_Percentage_As_Integer()
+{
+	int BatteryPercent = 0;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		BatteryPercent = static_cast<int>(spsPwr.BatteryLifePercent);
+	}
+	return BatteryPercent;
+}
+double WindowsBattery::Output_Is_Computer_Plugged_In_As_Double()
+{
+
+	double IsPCPluggedIn = false;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		IsPCPluggedIn = static_cast<double>(spsPwr.ACLineStatus);
+	}
+	return IsPCPluggedIn;
+}
+int WindowsBattery::Output_Is_Computer_Plugged_In_As_Integer()
+{
+
+	int IsPCPluggedIn = false;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		IsPCPluggedIn = static_cast<int>(spsPwr.ACLineStatus);
+	}
+	return IsPCPluggedIn;
+}
+bool WindowsBattery::Output_Is_Computer_Plugged_In_As_Boolean()
+{
+
+	bool IsPCPluggedIn = false;
+	SYSTEM_POWER_STATUS spsPwr;
+	if (GetSystemPowerStatus(&spsPwr)) {
+		IsPCPluggedIn = static_cast<bool>(spsPwr.ACLineStatus);
+	}
+	return IsPCPluggedIn;
+}
--- LanguageEasy.h
+++ LanguageEasy.h
@@ -1,5 +1,10 @@
+#ifndef LANGUAGEEASY_H
+#define LANGUAGEEASY_H
+
+
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <Windows.h>
 
 using namespace std;
@@ -34,5 +39,30 @@ public:
 private:
 	string Message;
 	string MessageName;
+
+};
+
+class WindowsBattery
+{
+public:
+	// Default Constructor
+	WindowsBattery(bool IsBatteryPresent);
+
+	//Accessors
+	string Output_Battery_Percentage_As_String();
+	double Output_Battery_Percentage_As_Double();
+	int Output_Battery_Percentage_As_Integer();
+	double Output_Battery_Flag_As_Double();
+	int Output_Battery_Flag_As_Integer();
+	bool Output_Battery_Flag_As_Boolean();
+	double Output_Is_Computer_Plugged_In_As_Double();
+	int Output_Is_Computer_Plugged_In_As_Integer();
+	bool Output_Is_Computer_Plugged_In_As_Boolean();
+	
+	
+	
+	
+
+private:
 
 };
